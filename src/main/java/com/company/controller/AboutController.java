ccb2119e2c2b61c9ba27bbec3ee905d4490dd8b7
levent yıldız <leventyildiz@leventyildiz.local>
2019-11-03 17:52:47 +0300

    add hibernate to project and simple insert from about page

--- src/main/java/com/company/controller/AboutController.java
+++ src/main/java/com/company/controller/AboutController.java
@@ -1,5 +1,8 @@
 package com.company.controller;
 
+import com.company.model.Person;
+import com.company.util.HibernateUtil;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,7 +17,18 @@ public class AboutController {
 
     @RequestMapping(value = "/about", method = RequestMethod.GET)
     public ModelAndView about() {
+
         ModelAndView mv = new ModelAndView("about");
+
+        Session session = HibernateUtil.getSession().openSession();
+
+        session.beginTransaction();
+        Person person = new Person();
+        person.setName("alioğlu");
+
+        session.save(person);
+        session.getTransaction().commit();
+
         return mv;
     }
 
